4d75e2f0a32a054fdc72e5cbf8e74d39054f4045
Ishaan Sharma <107057912+Isharma-mi@users.noreply.github.com>
2024-07-18 20:08:35 -0400

    Updated UserInterface
    - Allows user to delete a list
    - TODO: Allow creation of items
    - TODO: Improve code consolidation

--- src/com/example/TodoList/ui/UserInterface.java
+++ src/com/example/TodoList/ui/UserInterface.java
@@ -2,6 +2,7 @@ package com.example.TodoList.ui;
 
 import com.example.TodoList.logic.ListOfItems;
 import com.example.TodoList.logic.ListOfLists;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class UserInterface {
@@ -37,34 +38,18 @@ public class UserInterface {
 				System.out.println("ERROR: Please give a correct input!");
 				continue;
 			} else if(input.equals("1")) { 
+				// Shows all created lists
 				viewAllLists();
 			}else if (input.equals("3")) {
 				// Creates list
 				createList();
+			} else if (input.equals("4")){
+				// Deletes a list
+				deleteList();
 			} else if(input.equals("6")) {
 				// Terminates program
 				break;
 			}
-		}
-	}
-	
-	/**
-	 * Used to make a new list based off user input.
-	 */
-	private void createList() {
-		System.out.println("What do you want the name of the list to be?");
-		String listName = this.scanner.nextLine();
-		
-		ListOfLists listOfListsRef = ListOfLists.getInstance();
-		
-		// Checks if the name already exists
-		boolean listExists = listOfListsRef.getListsOfItems().stream().anyMatch(obj -> obj.getName().equals(listName));
-		
-		if (listExists) { 
-			System.out.println("ERROR: List with same exists already!");
-		} else {
-			// Creates list and adds it to list of lists
-			listOfListsRef.addList(new ListOfItems(listName));
 		}
 	}
 	
@@ -78,5 +63,54 @@ public class UserInterface {
 		for (ListOfItems l: ListOfLists.getInstance().getListsOfItems()) {
 			System.out.println("\t" + l);
 		}
+	}
+	
+	/**
+	 * Used to make a new list based off user input.
+	 */
+	private void createList() {
+		System.out.println("What do you want the name of the list to be?");
+		String listName = this.scanner.nextLine().trim();
+		
+		ListOfLists listOfListsRef = ListOfLists.getInstance();
+		
+		// Checks if a list with the same name already exists
+		boolean listExists = listOfListsRef.getListsOfItems().stream().anyMatch(obj -> obj.getName().equals(listName));
+		
+		if (listExists) { 
+			System.out.println("ERROR: List with same exists already!");
+		} else {
+			// Creates list and adds it to list of lists
+			listOfListsRef.addList(new ListOfItems(listName));
+		}
+	}
+	
+	/**
+	 * Used to delete a list based off user input. Iterates thru list looking for one with the name that user gives.
+	 */
+	private void deleteList() {
+		System.out.println("What list do you want to delete?");
+		String listName = this.scanner.nextLine().trim();
+		
+		// Iterator chosen over for loop to perform safe removal
+		Iterator<ListOfItems> it = ListOfLists.getInstance().getListsOfItems().iterator();
+		boolean listDeleted = false;
+		
+		// Iterates thru all the lists till it finds the one user wants to delete
+		while (it.hasNext()) {
+			if (it.next().getName().equals(listName)) {
+				it.remove();
+				listDeleted = true;
+				break;
+			}
+		}
+		
+		// Lets user know if list was deleted or not
+		if (listDeleted) {
+			System.out.println("List successfuly deleted!");
+		} else {
+			System.out.println("ERROR: List could NOT be found");
+		}
+
 	}
 }
