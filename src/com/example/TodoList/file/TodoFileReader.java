be4fad2f974afd61fc81a30327a740bc6a9088bd
Ishaan Sharma <107057912+Isharma-mi@users.noreply.github.com>
2024-07-24 16:52:56 -0400

    Importing list files
    - Method to read from a file works (Needs to be in format of exported file)
    - Added txt file for testing import
    - TODO: Implement it into program so user can use that method
    - TODO: Prevent blank named lists or items from being made
    - TODO: Prevent list names starting with dash (-)
    -

---
+++ src/com/example/TodoList/file/TodoFileReader.java
@@ -0,0 +1,74 @@
+package com.example.TodoList.file;
+
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import com.example.TodoList.logic.ListOfLists;
+
+public class TodoFileReader {
+	
+	public static boolean importFile(String fileName) {
+		boolean fileRead = false;
+		try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+			boolean inAList = false;
+			String line;
+			String listName = null;
+			String itemName = null;
+			String description = "";
+			
+			while((line = reader.readLine()) != null) {
+				if (!inAList) {
+					// If the current line is a list name
+					
+					if (!line.startsWith("-") && !line.startsWith("\t") && !line.isEmpty()) {
+						// Creates a ListofItems obj
+						ListOfLists.getInstance().addList(line);
+						listName = line;
+						inAList = true;
+						continue;
+					}
+				}
+
+				if (inAList) {
+					// If going thru a list
+					if (line.startsWith("-")) {
+						// If the line is an item
+						if (itemName != null) {
+							/* 
+							 * Adds items from the second item to the last item
+							 * Specifically once a new item name is read 
+							 */
+							ListOfLists.getInstance().getList(listName).addItem(itemName, description);
+						}
+						
+						// Removes the - and space before the item's name
+						itemName = line.substring(2);
+						description = "";
+					} else if (line.startsWith("\t")){
+						// Removes the tab spacing before the description
+						description = line.trim();
+					} else {
+						 // Adds the last item in the list
+						ListOfLists.getInstance().getList(listName).addItem(itemName, description);
+
+						// Resets item name so that last item of previous list does not get added in the next list
+						itemName = null;
+						inAList = false;
+					}
+				}
+			}
+			fileRead = true;
+		} catch (IOException e) {
+			System.out.println("ERROR: Unable to read file!");
+		}
+		
+		/* FOR TESTING PURPOSES 
+		System.out.println(ListOfLists.getInstance().getList("Lightsabers"));
+		System.out.println(ListOfLists.getInstance().getList("Groceries"));
+		System.out.println(ListOfLists.getInstance().getList("Weapons"));
+		*/
+		
+		return fileRead;
+	}		
+}
--- src/com/example/TodoList/logic/Main.java
+++ src/com/example/TodoList/logic/Main.java
@@ -2,6 +2,7 @@ package com.example.TodoList.logic;
 
 import com.example.TodoList.ui.UIController;
 
+
 public class Main {
 	
 	public static void main(String[] args) {
